b940595e3171d82715fadbf4b01e2c2cf9b4f922
Maxim Muzafarov <m.muzafarov@gmail.com>
2015-04-01 10:59:56 +0500

    serverMVC.py

--- serverMVC.py
+++ serverMVC.py
@@ -3,14 +3,16 @@ __author__ = 'm_messiah'
 from wsgiref.simple_server import make_server
 import appMVC as app
 
+PORT = 8051
+
 # Instantiate the WSGI server.
 # It will receive the request, pass it to the application
 # and send the application's response to the client
 httpd = make_server(
     '0.0.0.0',
-    8051,
+    PORT,
     app.application
 )
 
-print "Run: http://0.0.0.0:8051/"
+print "Run: http://0.0.0.0:%s/" % PORT
 httpd.serve_forever()
